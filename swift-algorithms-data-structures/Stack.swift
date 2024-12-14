0d8c8c8ac4c35c25d51c950040c9f34ab49092a0
Amin Faruq <aminfaruk.fa@gmail.com>
2024-12-14 14:14:12 +0800

    feat: Stack push

--- swift-algorithms-data-structures/Stack.swift
+++ swift-algorithms-data-structures/Stack.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct Stack<Element> {
+    // Storage
+    private var storage: [Element] = []
+    
+    // Push
+    mutating func push(_ element: Element) {
+        storage.append(element)
+    }
+    
+    // Pop
+    
+}
+
+extension Stack: CustomStringConvertible {
+    var description: String {
+        return storage
+            .map { "\($0)" }
+            .joined(separator: " ")
+    }
+}
--- swift-algorithms-data-structuresTests/StackTest.swift
+++ swift-algorithms-data-structuresTests/StackTest.swift
@@ -9,5 +9,16 @@ import XCTest
 @testable import swift_algorithms_data_structures
 
 final class StackTest: XCTestCase {
-
+    var stack = Stack<Int>()
+    
+    override func setUp() {
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        stack.push(4)
+    }
+    
+    func test_push() {
+        XCTAssertEqual(stack.description, "1 2 3 4")
+    }
 }
